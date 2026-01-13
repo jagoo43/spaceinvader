656c8a170bb3efc6fd044d40ef39b960b301f251
jagoo43 <jagoogamer1@gmail.com>
2026-01-13 18:02:43 +0530

    Update test110.py

--- test110.py
+++ test110.py
@@ -1,73 +1,71 @@
-import math
-import random
-import pygame
+import math,random,pygame
 SCREENWIDTH=800
-SCREENHIEGHT=500
+SCREENHEIGHT=500
 FPS=60
 PLAYERWIDTH=64
-PLAYERHIEGHT=64
+PLAYERHEIGHT=64
 PLAYERSPEED=3
 ENEMYWIDTH=64
-ENEMYHIEGHT=64
+ENEMYHEIGHT=64
 BULLETWIDTH=32
-BULLETHIEGHT=32
+BULLETHEIGHT=32
 PLAYERSTARTX=370
 PLAYERSTARTY=380
 ENEMYSTARTYMIN=50
-ENEMYSTARTMAX=150
+ENEMYSTARTYMAX=150
 ENEMYSPEEDX=2
 ENEMYSPEEDY=40
 BULLETSPEEDY=6
-COLLISONDITANCE=27
+COLLISIONDISTANCE=27
 pygame.init()
-screen=pygame.display.set_mode((SCREENWIDTH,SCREENHIEGHT))
+screen=pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
 pygame.display.set_caption("Space Invader Game")
 clock=pygame.time.Clock()
-background=pygame.image.load("bg1.png")
-background=pygame.transform.scale(background,(SCREENWIDTH,SCREENHIEGHT))
+background=pygame.image.load("img34.png")
+background=pygame.transform.scale(background,(SCREENWIDTH,SCREENHEIGHT))
 playerimg=pygame.image.load("spaceship.png")
-playerimg=pygame.transform.scale(playerimg(PLAYERWIDTH,PLAYERHIEGHT))
-playerx=PLAYERSTARTX
-playery=PLAYERSTARTY
-playerxchange=0
+playerimg=pygame.transform.scale(playerimg,(PLAYERWIDTH,PLAYERHEIGHT))
 enemyimg=[]
 enemyx=[]
-enemyY=[]
+enemyy=[]
 enemyxchange=[]
 enemyychange=[]
 numofenemies=6
 for i in range(numofenemies):
     img=pygame.image.load("alien.png")
-    img=pygame.transform.scale(img,ENEMYWIDTH,ENEMYHIEGHT)
+    img=pygame.transform.scale(img,(ENEMYWIDTH,ENEMYHEIGHT))
     enemyimg.append(img)
-    enemyx.append(random.randint(0,SCREENWIDTH=ENEMYWIDTH))
-    enemyY.append(random.randint(ENEMYSTARTYMIN,ENEMYSTARTMAX))
+    enemyx.append(random.randint(0,SCREENWIDTH-ENEMYWIDTH))
+    enemyy.append(random.randint(ENEMYSTARTYMIN,ENEMYSTARTYMAX))
     enemyxchange.append(ENEMYSPEEDX)
     enemyychange.append(ENEMYSPEEDY)
 bulletimg=pygame.image.load("bullet.png")
-bulletimg=pygame.transform.scale(bulletimg,(BULLETWIDTH,BULLETHIEGHT))
+bulletimg=pygame.transform.scale(bulletimg,(BULLETWIDTH,BULLETHEIGHT))
 bulletx=0
-bullety=PLAYERSTARTX
+bullety=PLAYERSTARTY
 bulletstate="ready"
+playerx=PLAYERSTARTX
+playery=PLAYERSTARTY
+playerxchange=0
 scorevalue=0
-font=pygame.font.Font("freesansbold.ttf",33)
-overfont=pygame.font.Font("freesansbold.ttf",63)
+font=pygame.font.Font("freesansbold.ttf",32)
+overfont=pygame.font.Font("freesansbold.ttf",64)
 def showscore(x,y):
     score=font.render(f"Score:{scorevalue}",True,(255,255,255))
     screen.blit(score,(x,y))
 def gameovertext():
     overtext=overfont.render("GAME OVER",True,(255,255,255))
+    screen.blit(overtext,(200,200))
 def player(x,y):
     screen.blit(playerimg,(x,y))
 def enemy(x,y,i):
-    screen.blit(enemyimg{i},(x,y))
-def firebullett(x,y):
+    screen.blit(enemyimg[i],(x,y))
+def firebullet(x,y):
     global bulletstate
     bulletstate="fire"
     screen.blit(bulletimg,(x+PLAYERWIDTH//2-BULLETWIDTH//2,y))
-def iscollision(enemyX,enemyY,bulletx,bullety):
-    distance=math.sqrt((enemyX-bulletx)**2+(enemyY-bullety)**2)
-    return distance<COLLISONDITANCE
+def iscollision(ex,ey,bx,by):
+    return math.sqrt((ex-bx)**2+(ey-by)**2)<COLLISIONDISTANCE
 running=True
 while running:
     clock.tick(FPS)
@@ -82,35 +80,37 @@ while running:
                 playerxchange=PLAYERSPEED
             if event.key==pygame.K_SPACE and bulletstate=="ready":
                 bulletx=playerx
-                firebullett(bulletx,bullety)
+                firebullet(bulletx,bullety)
         if event.type==pygame.KEYUP:
-            if event.key in (pygame.K_LEFT,pygame.K_RIGHT):
+            if event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT:
                 playerxchange=0
     playerx+=playerxchange
-    playerx=max(0,min(playerx,SCREENWIDTH-PLAYERWIDTH))
+    if playerx<0:playerx=0
+    if playerx>SCREENWIDTH-PLAYERWIDTH:playerx=SCREENWIDTH-PLAYERWIDTH
     for i in range(numofenemies):
-        if enemyY[i]>340:
+        if enemyy[i]>340:
             for j in range(numofenemies):
-                enemyY[j]=2000
+                enemyy[j]=2000
             gameovertext()
             break
-        enemy[i]+=enemyxchange[i]
-        if enemy[i]<=0 or enemyx[i]>=SCREENWIDTH-ENEMYWIDTH:
+        enemyx[i]+=enemyxchange[i]
+        if enemyx[i]<=0 or enemyx[i]>=SCREENWIDTH-ENEMYWIDTH:
             enemyxchange[i]*=-1
-            enemyY[i]+=enemyychange[i]
-        if iscollision(enemyx[i],enemyY[i],bulletx,bullety):
+            enemyy[i]+=enemyychange[i]
+        if iscollision(enemyx[i],enemyy[i],bulletx,bullety):
             bullety=PLAYERSTARTY
             bulletstate="ready"
             scorevalue+=1
             enemyx[i]=random.randint(0,SCREENWIDTH-ENEMYWIDTH)
-            enemyY[i]=random.randint(ENEMYSTARTYMIN,ENEMYSTARTMAX)
-        enemy(enemyx[i],enemyY[i],i)
+            enemyy[i]=random.randint(ENEMYSTARTYMIN,ENEMYSTARTYMAX)
+        enemy(enemyx[i],enemyy[i],i)
     if bullety<=0:
         bullety=PLAYERSTARTY
         bulletstate="ready"
-    elif bulletstate=="fire":
-        firebullett(bulletx,bullety)
+    if bulletstate=="fire":
+        firebullet(bulletx,bullety)
         bullety-=BULLETSPEEDY
     player(playerx,playery)
     showscore(10,10)
-    pygame.display.update
+    pygame.display.update()
+pygame.quit()
